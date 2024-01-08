ba7485afc45e4b8ed26a45228fead45a9f2e0233
Sreejith Nair <hisreenair@gmail.com>
2024-01-07 21:53:09 -0600

    Updates to html

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        backimg: "url('./assets/images/background.jpg')",
+      },
+    },
   },
   plugins: [],
 };
